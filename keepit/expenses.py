c84cafe2a1b03e9dd1bcbdc69e790baeb454e2e1
franklinmatheus <franklinmatheus1@gmail.com>
2019-06-13 03:34:27 -0300

    [Despesa comum] todas funcionalidades implementadas

--- keepit/expenses.py
+++ keepit/expenses.py
@@ -1,5 +1,5 @@
 from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
-from keepit.db import get_db, insert_expense_common, select_expense_common, remove_resource
+from keepit.db import get_db, insert_expense_common, select_expense_common, update_common_expenses, update_common_expense_constant, update_common_expense_inconstant, remove_resource, cancel_resource
 from keepit.auth import login_required
 import datetime
 
@@ -17,10 +17,9 @@ def common():
         name = request.form['name']
         value = request.form['value']
         month_day = request.form['month_day']
-        payment_date = request.form['payment_date']
-        cancelation_date = request.form['cancelation_date']
         status = 1
-        anotation_date = datetime.datetime.now().strftime("%Y-%m-%d")
+        anotation_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        payment_date = None
         error = None
 
         automatic = 0
@@ -32,30 +31,68 @@ def common():
 
         if month_day == '':
             error = 'Inform a month day'
-        elif payment_date == '':
-            error = 'Inform a payment date'
-        elif cancelation_date == '':
-            error = 'Inform a cancelation date'
-        
-        data = {'name':name,'value':value,'month_day':month_day,
-            'payment_date':payment_date,'anotation_date':anotation_date,
-            'cancelation_date':cancelation_date,'automatic':automatic,
-            'constant':constant,'status':status}
 
-        insert_expense_common(session.get('user_id'),data)
+        if constant == 0 and automatic == 1:
+            error = 'A common expense can not be automatic and inconstant'
         
-        if error is not None:
+        if error is None:
+            if automatic == 1:
+                temp_date = datetime.datetime.now()
+                temp_day = int(month_day)
+                if temp_day < temp_date.day:
+                    payment_date = datetime.datetime(temp_date.year, temp_date.month, temp_day)
+                elif temp_day > temp_date.day:
+                    if temp_date.month == 1:
+                        payment_date = datetime.datetime(temp_date.year-1, 12, temp_day)
+                    else:
+                        payment_date = datetime.datetime(temp_date.year, temp_date.month-1, temp_day)
+                else:
+                    payment_date = datetime.datetime.now().strftime("%Y-%m-%d")
+            else:
+                status = 0
+               
+            data = {'name':name,'value':value,'month_day':month_day,
+                'payment_date':payment_date,'anotation_date':anotation_date,
+                'cancelation_date':None,'automatic':automatic,
+                'constant':constant,'status':status}
+
+            insert_expense_common(session.get('user_id'),data)
+        
+        elif error is not None:
             flash(error)
 
     common_expenses = select_expense_common(session.get('user_id'))
     return render_template('restrict/expenses/common.html',common_expenses=common_expenses)
 
-@bp.route('/common/<int:id>', methods=['POST'])
+@bp.route('/common/<int:id>/delete', methods=['POST'])
 @login_required
 def delete_common(id):
     if request.method == 'POST':
         remove_resource(id)
     return redirect(url_for('expenses.common'))
+
+@bp.route('/common/<int:id>/cancel', methods=['POST'])
+@login_required
+def cancel_common(id):
+    if request.method == 'POST':
+        cancel_resource(id,datetime.datetime.now().strftime("%Y-%m-%d"))
+    return redirect(url_for('expenses.common'))
+
+@bp.route('/common/<int:id>/update/constant', methods=['POST'])
+@login_required
+def update_common_constant(id):
+    if request.method == 'POST':
+        update_common_expense_constant(id)
+    return redirect(url_for('expenses.common'))
+
+@bp.route('/common/<int:id>/update/inconstant', methods=['POST'])
+@login_required
+def update_common_inconstant(id):
+    if request.method == 'POST':
+        value = request.form['value']
+        update_common_expense_inconstant(id,value)
+    return redirect(url_for('expenses.common'))
+
 
 @bp.route('/uncommon', methods=('GET', 'POST'))
 @login_required
@@ -71,3 +108,11 @@ def estimated():
 @login_required
 def programmed():
     return render_template('restrict/expenses/programmed.html')
+
+
+'''
+Before requests
+'''
+@bp.before_app_request
+def check_common_expenses():
+    update_common_expenses(session.get('user_id'),'2019-06-29')
