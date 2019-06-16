a1671316a91092b05a5f526c2d8bf80ab00482aa
franklinmatheus <franklinmatheus1@gmail.com>
2019-06-16 02:17:14 -0300

    [Analise] Calendario e grafico de despesas/receitas

--- keepit/restrict.py
+++ keepit/restrict.py
@@ -1,10 +1,25 @@
 from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
-from keepit.db import get_db
+from keepit.db import update_common_revenues, update_common_expenses
+from keepit.db_analysis import get_balance, get_expenses_info, get_resources_info
 from keepit.auth import login_required
+import datetime
 
 bp = Blueprint('restrict',__name__,url_prefix='/restrict')
 
 @bp.route('/', methods=('GET', 'POST'))
 @login_required
 def home():
-	return render_template('restrict/index.html')
+    curr_date = datetime.datetime.now()
+    balance = get_balance(session.get('user_id'))
+    expenses_info = get_expenses_info(session.get('user_id'),curr_date.month,curr_date.year)
+    revenues_info = get_resources_info(session.get('user_id'),curr_date.month,curr_date.year)
+    curr_month = curr_date.strftime('%B') + ' ' + str(curr_date.year)
+    return render_template('restrict/index.html',balance=balance,expenses_info=expenses_info,revenues_info=revenues_info,curr_month=curr_month)
+
+'''
+Before requests
+'''
+@bp.before_app_request
+def check_common_resources():
+    update_common_revenues(session.get('user_id'),datetime.datetime.now().strftime("%Y-%m-%d"))
+    update_common_expenses(session.get('user_id'),datetime.datetime.now().strftime("%Y-%m-%d"))
